355dcd57fdbf803e0e02ea9881ad1cc49294732d
Vittoria2555 <vitto.tonini@gmail.com>
2025-10-23 00:01:34 +0200

    add API controllers for Allergen

---
+++ WebAPIExercise/Controllers/AllergensController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIExercise.Models;
+
+namespace WebApiExercise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AllergensController : Controller
+    {
+        public List<Allergen> allergenList; //dichiarazione della lista
+
+
+        public AllergensController() //istanziamo la lista nel costruttore
+        {
+            allergenList = new List<Allergen>()
+            {
+                new Allergen { Id = 1, Name = "Milk"},
+                new Allergen { Id = 2, Name = "Eggs"},
+                new Allergen { Id = 3, Name = "Gluten"}
+            };
+        }
+
+        // GET: AllergensController
+        [HttpGet]
+        public IEnumerable<Allergen> Index()
+        {
+            return allergenList;
+        }
+
+        // GET: AllergensController/search
+        [HttpGet("search")]
+        public IActionResult Get(string name)
+        {
+            var result = allergenList.FirstOrDefault(a => a.Name == name);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
